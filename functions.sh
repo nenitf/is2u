af0ee6bf3dcfd3354319d77ad7defe2af3eeba94
Felipe Silva <felipe.dacs@gmail.com>
2019-07-24 03:40:55 -0300

    👌 IMPROVE: Instalação openssh para uso com git

--- functions.sh
+++ functions.sh
@@ -13,16 +13,16 @@ INTERFACE_GRAFICA=$XDG_CURRENT_DESKTOP  # LXDE
 declare -a ARR_INSTALL_OF_PPA=()
 # evitar multiplos updates desnecessarios
 install_all_from_ppa(){
-    sudo apt-get update
-    for i in "${ARR_INSTALL_OF_PPA[@]}"
-    do
-        echo `$i`
-    done
+  sudo apt-get update
+  for i in "${ARR_INSTALL_OF_PPA[@]}"
+  do
+    echo `$i`
+  done
 }
 
 # Caixas de dialogo do script
 install_whiptail(){
-    sudo apt-get install -y whiptail
+  sudo apt-get install -y whiptail
 }
 
 ###########################
@@ -30,12 +30,12 @@ install_whiptail(){
 ###########################
 
 install_suckless(){
-    rm -r -f $DIR_INSTALLATION/tmp/suckless
-    mkdir -p $DIR_INSTALLATION/tmp/suckless
+  rm -r -f $DIR_INSTALLATION/tmp/suckless
+  mkdir -p $DIR_INSTALLATION/tmp/suckless
 
-    sudo apt install -y build-essential				            # gcc, make and other tools for sources compiling
-    sudo apt install -y libx11-dev libxinerama-dev libxft-dev	# Development headers, required for dwm and dmenu building
-    sudo apt install -y libxinerama1   # Xinerama extension for X protocol (multiple screens attached to a single display)
+  sudo apt install -y build-essential				            # gcc, make and other tools for sources compiling
+  sudo apt install -y libx11-dev libxinerama-dev libxft-dev	# Development headers, required for dwm and dmenu building
+  sudo apt install -y libxinerama1   # Xinerama extension for X protocol (multiple screens attached to a single display)
 
     # st #
     # dependencias st
@@ -57,172 +57,176 @@ install_suckless(){
     git clone https://github.com/nenitf/dwm.git $DIR_INSTALLATION/tmp/suckless/dwm
     cd $DIR_INSTALLATION/tmp/suckless/dwm
     sudo make clean install
-}
+  }
 
 install_dotfiles(){
-    wget -O - http://neni.dev/dotfiles/lazy.sh | sh
+  wget -O - http://neni.dev/dotfiles/lazy.sh | sh
 }
 
 # Mail client
 install_neomutt(){
-    sudo apt-get install -y neomutt
+  sudo apt-get install -y neomutt
 }
 
 install_xclip(){
-    sudo apt-get install -y xclip
+  sudo apt-get install -y xclip
 }
 
 install_htop(){
-    sudo apt-get install -y htop
+  sudo apt-get install -y htop
 }
 
 install_unrar(){
-    sudo apt-get install unrar
+  sudo apt-get install unrar
 }
 
 install_imagemagick(){
-    sudo apt-get install -y imagemagick
+  sudo apt-get install -y imagemagick
 }
 
 # Player de vídeo com youtube
 install_mpv(){
-    sudo apt-get install -y mpv
-    # youtube dl to play in mpv....
-    sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-    sudo chmod a+rx /usr/local/bin/youtube-dl
+  sudo apt-get install -y mpv
+  # youtube dl to play in mpv....
+  sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+  sudo chmod a+rx /usr/local/bin/youtube-dl
 }
 
 # Biblioteca de musicas
 install_cmus(){
-    sudo apt-get install -y cmus
+  sudo apt-get install -y cmus
 }
 
 # Editor de tags de musica
 install_id3v2(){
-    sudo apt-get install -y id3v2
+  sudo apt-get install -y id3v2
 }
 
 # Screen recorder
 install_ffmpeg(){
-    sudo apt-get install -y ffmpeg
+  sudo apt-get install -y ffmpeg
 }
 
 # Interpretador de .dot .gv
 install_graphviz(){
-    sudo apt-get install -y graphviz
+  sudo apt-get install -y graphviz
 }
 
 install_xorg(){
-    sudo apt install -y xorg
+  sudo apt install -y xorg
 }
 
 # Transpilador de markdown para pdf
 install_pandoc(){
-    sudo apt-get install -y pandoc
+  sudo apt-get install -y pandoc
 }
 
 # Uteis para manutenção de rede/internet
 install_conectivity(){
-    sudo apt-get install -y network-manager network-manager-gnome
-    # iwconfig
-    sudo apt-get install -y wireless-tools
-    # ifconfig
-    sudo apt-get install -y net-tools
-    # gtk janela de wifi
-    #sudo apt-get install -y wicd-gtk
+  sudo apt-get install -y network-manager network-manager-gnome
+  # iwconfig
+  sudo apt-get install -y wireless-tools
+  # ifconfig
+  sudo apt-get install -y net-tools
+  # gtk janela de wifi
+  #sudo apt-get install -y wicd-gtk
 }
 
 install_config_audio(){
-    sudo apt-get install -y pavucontrol pulseaudio-module-x11 pulseaudio csound-utils
+  sudo apt-get install -y pavucontrol pulseaudio-module-x11 pulseaudio csound-utils
 }
 
 install_arandr(){
-    sudo apt-get install -y arandr
+  sudo apt-get install -y arandr
 }
 
 # Visualizador de imagens e gerenciador de background
 install_feh(){
-    sudo apt-get install -y feh
+  sudo apt-get install -y feh
 }
 
 install_curl(){
-    sudo apt-get install -y curl
+  sudo apt-get install -y curl
 }
 
 # Icones svg no sistena
 install_all_fonts(){
-    git clone https://github.com/ryanoasis/nerd-fonts.git $DIR_INSTALLATION/tmp/nerd-fonts
-    .$DIR_INSTALLATION/tmp/nerd-fonts/install.sh
+  git clone https://github.com/ryanoasis/nerd-fonts.git $DIR_INSTALLATION/tmp/nerd-fonts
+  .$DIR_INSTALLATION/tmp/nerd-fonts/install.sh
 }
 
 install_nvim(){
-    sudo add-apt-repository ppa:neovim-ppa/stable -y
-    #sudo apt-get update
-    #sudo apt-get install -y neovim
-    ARR_INSTALL_OF_PPA=("sudo apt-get install -y neovim")
-    sudo apt-get install -y exuberant-ctags
-    # neovim como editor sempre que possivel
-    # https://github.com/neovim/neovim/wiki/Installing-Neovim
-    sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-    sudo update-alternatives --config vi --skip-auto
-    sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-    sudo update-alternatives --config vim --skip-auto
-    sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-    sudo update-alternatives --config editor --skip-auto
-    nvim -c PlugInstall -c qall teste.txt
+  sudo add-apt-repository ppa:neovim-ppa/stable -y
+  #sudo apt-get update
+  #sudo apt-get install -y neovim
+  ARR_INSTALL_OF_PPA=("sudo apt-get install -y neovim")
+  sudo apt-get install -y exuberant-ctags
+  # neovim como editor sempre que possivel
+  # https://github.com/neovim/neovim/wiki/Installing-Neovim
+  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+  sudo update-alternatives --config vi --skip-auto
+  sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+  sudo update-alternatives --config vim --skip-auto
+  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+  sudo update-alternatives --config editor --skip-auto
+  nvim -c PlugInstall -c qall teste.txt
 }
 
 install_termlab(){
-    curl -s https://raw.githubusercontent.com/lighttiger2505/lab/master/install.sh | bash
+  curl -s https://raw.githubusercontent.com/lighttiger2505/lab/master/install.sh | bash
 }
 
 install_termhub(){
-    mkdir -p "$GOPATH"/src/github.com/github
-    git clone --config transfer.fsckobjects=false --config receive.fsckobjects=false --config fetch.fsckobjects=false https://github.com/github/hub.git "$GOPATH"/src/github.com/github/hub
-    cd "$GOPATH"/src/github.com/github/hub
-    sudo make install prefix=/usr/local
+  mkdir -p "$GOPATH"/src/github.com/github
+  git clone --config transfer.fsckobjects=false --config receive.fsckobjects=false --config fetch.fsckobjects=false https://github.com/github/hub.git "$GOPATH"/src/github.com/github/hub
+  cd "$GOPATH"/src/github.com/github/hub
+  sudo make install prefix=/usr/local
 }
 
 install_browsers(){
-    sudo apt install -y firefox
-    sudo apt install -y qutebrowser
-    sudo apt install -y surf
+  sudo apt install -y firefox
+  sudo apt install -y qutebrowser
+  sudo apt install -y surf
 }
 
 # File manager
 install_fff(){
-    # sudo apt-get install -y xdotool w3m-img # feh is better!
-    git clone https://github.com/dylanaraps/fff $DIR_INSTALLATION/tmp/fff
-    cd $DIR_INSTALLATION/tmp/fff
-    sudo make install
+  # sudo apt-get install -y xdotool w3m-img # feh is better!
+  git clone https://github.com/dylanaraps/fff $DIR_INSTALLATION/tmp/fff
+  cd $DIR_INSTALLATION/tmp/fff
+  sudo make install
 }
 
 install_atom(){
-    sudo add-apt-repository ppa:webupd8team/atom -y
-    #sudo apt-get update
-    #sudo apt-get install -y atom
-    ARR_INSTALL_OF_PPA=("sudo apt-get install -y atom")
+  sudo add-apt-repository ppa:webupd8team/atom -y
+  #sudo apt-get update
+  #sudo apt-get install -y atom
+  ARR_INSTALL_OF_PPA=("sudo apt-get install -y atom")
 }
 
 install_zathura(){
-    # pdf, cbr (comics), post script and djvu
-    sudo apt-get install -y zathura zathura-cb zathura-ps zathura-djvu
+  # pdf, cbr (comics), post script and djvu
+  sudo apt-get install -y zathura zathura-cb zathura-ps zathura-djvu
+}
+
+install_openssh(){
+  sudo apt install openssh-client
 }
 
 install_git_flow(){
-    sudo apt-get install -y git-flow
+  sudo apt-get install -y git-flow
 }
 
 install_docker(){
-    # docker instalavel somente em x64
-    if [ ${ARQUITETURA} == 'x86_64' ]; then
-        log_info_action "INSTALANDO DOCKER"
-        sudo apt-get install -y apt-transport-https ca-certificates gnupg-agent
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-        sudo apt-get update
-        sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-    fi
+  # docker instalavel somente em x64
+  if [ ${ARQUITETURA} == 'x86_64' ]; then
+    log_info_action "INSTALANDO DOCKER"
+    sudo apt-get install -y apt-transport-https ca-certificates gnupg-agent
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+  fi
 }
 
 
@@ -231,8 +235,8 @@ install_docker(){
 ###########################
 
 install_jdk(){
-    sudo -E apt-get install -y oracle-java8-installer
-    sudo -E apt-get install -y oracle-java8-set-default
+  sudo -E apt-get install -y oracle-java8-installer
+  sudo -E apt-get install -y oracle-java8-set-default
 }
 
 
@@ -241,7 +245,7 @@ install_jdk(){
 ###########################
 
 install_node(){
-    wget -qO- https://deb.nodesource.com/setup_8.x | sudo -E bash -
+  wget -qO- https://deb.nodesource.com/setup_8.x | sudo -E bash -
 }
 
 
@@ -250,13 +254,13 @@ install_node(){
 ###########################
 
 install_php(){
-    sudo apt-get install -y php
+  sudo apt-get install -y php
 }
 
 install_composer(){
-    # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-composer-on-ubuntu-14-04
-    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-    sudo chown -R $USER $HOME/.composer
+  # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-composer-on-ubuntu-14-04
+  curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+  sudo chown -R $USER $HOME/.composer
 }
 
 
@@ -265,11 +269,11 @@ install_composer(){
 ###########################
 
 install_python3(){
-    sudo apt-get install python-software-properties
-    sudo apt-get install -y python-dev python-pip python3-dev python3 python3-pip python3-setuptools cmake
-    sudo apt install -y python3-flask
-    pip3 install flask
-    pip3 install --user pynvim
+  sudo apt-get install python-software-properties
+  sudo apt-get install -y python-dev python-pip python3-dev python3 python3-pip python3-setuptools cmake
+  sudo apt install -y python3-flask
+  pip3 install flask
+  pip3 install --user pynvim
 }
 
 
@@ -278,8 +282,8 @@ install_python3(){
 ###########################
 
 install_go(){
-    #sudo add-apt-repository ppa:longsleep/golang-backports
-    sudo apt-get install -y golang-go
+  #sudo add-apt-repository ppa:longsleep/golang-backports
+  sudo apt-get install -y golang-go
 }
 
 
@@ -288,7 +292,7 @@ install_go(){
 ###########################
 
 install_latex(){
-    sudo apt-get install -y texlive-full
+  sudo apt-get install -y texlive-full
 }
 
 
@@ -297,33 +301,33 @@ install_latex(){
 ###########################
 
 install_discord(){
-    #https://www.edivaldobrito.com.br/discord-no-ubuntu-debian-mint/
-    wget "https://discordapp.com/api/download?platform=linux&format=deb" -O discord.deb
-    sudo dpkg -i discord.deb
-    sudo apt-get install -f
-    rm discord.deb
+  #https://www.edivaldobrito.com.br/discord-no-ubuntu-debian-mint/
+  wget "https://discordapp.com/api/download?platform=linux&format=deb" -O discord.deb
+  sudo dpkg -i discord.deb
+  sudo apt-get install -f
+  rm discord.deb
 }
 
 install_calibre(){
-    #https://www.edivaldobrito.com.br/instalar-o-calibre-no-linux/
-    sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+  #https://www.edivaldobrito.com.br/instalar-o-calibre-no-linux/
+  sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 }
 
 install_inkscape(){
-    sudo add-apt-repository ppa:inkscape.dev/stable -y
-    #sudo apt-get update
-    #sudo apt-get install -y inkscape
-    ARR_INSTALL_OF_PPA=("sudo apt-get install -y inkscape")
+  sudo add-apt-repository ppa:inkscape.dev/stable -y
+  #sudo apt-get update
+  #sudo apt-get install -y inkscape
+  ARR_INSTALL_OF_PPA=("sudo apt-get install -y inkscape")
 }
 
 install_gimp(){
-    sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y
-    #sudo apt-get update
-    #sudo apt-get install -y gimp gimp-gmic gmic
-    #sudo apt-get install -y gimp-plugin-registry
-    ARR_INSTALL_OF_PPA=("sudo apt-get install -y gimp gimp-gmic gmic gimp-plugin-registry")
+  sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y
+  #sudo apt-get update
+  #sudo apt-get install -y gimp gimp-gmic gmic
+  #sudo apt-get install -y gimp-plugin-registry
+  ARR_INSTALL_OF_PPA=("sudo apt-get install -y gimp gimp-gmic gmic gimp-plugin-registry")
 }
 
 install_transmission(){
-    sudo apt-get install -y transmission-gtk
+  sudo apt-get install -y transmission-gtk
 }
--- install.sh
+++ install.sh
@@ -27,160 +27,163 @@ DIR_INSTALLATION=$HOME/dev/is
 
 # ------------------------- FUNÇÕES ------------------------ #
 option_base(){
-    log_info_option "BASE"
+  log_info_option "BASE"
 
-    # para poder add ppa
-    sudo apt-get install -y software-properties-common
+  # para poder add ppa
+  sudo apt-get install -y software-properties-common
 
-    sudo apt-get install -y ubuntu-drivers-common
-    sudo apt-get install -y libnm-gtk-common
+  sudo apt-get install -y ubuntu-drivers-common
+  sudo apt-get install -y libnm-gtk-common
 
-    # local onde serão clonados repositorios
-    mkdir -p $DIR_INSTALLATION/tmp
+  # local onde serão clonados repositorios
+  mkdir -p $DIR_INSTALLATION/tmp
 
-    log_info_action "LINKANDO DOTFILES"
-    install_dotfiles
+  log_info_action "LINKANDO DOTFILES"
+  install_dotfiles
 
-    log_info_action "INSTALANDO NEOMUTT"
-    install_neomutt
+  log_info_action "INSTALANDO NEOMUTT"
+  install_neomutt
 
-    log_info_action "INSTALANDO HTOP"
-    install_htop
+  log_info_action "INSTALANDO HTOP"
+  install_htop
 
-    log_info_action "COMPILANDO SUCKLESS"
-    install_suckless
+  log_info_action "COMPILANDO SUCKLESS"
+  install_suckless
 
-    log_info_action "INSTALANDO IMAGEMAGICK"
-    install_imagemagick
+  log_info_action "INSTALANDO IMAGEMAGICK"
+  install_imagemagick
 
-    log_info_action "INSTALANDO MPV"
-    install_mpv
+  log_info_action "INSTALANDO MPV"
+  install_mpv
 
-    log_info_action "INSTALANDO CMUS"
-    install_cmus
+  log_info_action "INSTALANDO CMUS"
+  install_cmus
 
-    log_info_action "INSTALANDO ID3V2"
-    install_id3v2
+  log_info_action "INSTALANDO ID3V2"
+  install_id3v2
 
-    log_info_action "INSTALANDO FFMPEG"
-    install_ffmpeg
+  log_info_action "INSTALANDO FFMPEG"
+  install_ffmpeg
 
-    log_info_action "INSTALANDO GRAPHVIZ"
-    install_graphviz
+  log_info_action "INSTALANDO GRAPHVIZ"
+  install_graphviz
 
-    log_info_action "INSTALANDO XORG"
-    install_xorg
+  log_info_action "INSTALANDO XORG"
+  install_xorg
 
-    log_info_action "INSTALANDO XCLIP"
-    install_xclip
+  log_info_action "INSTALANDO XCLIP"
+  install_xclip
 
-    log_info_action "INSTALANDO PANDOC"
-    install_pandoc
+  log_info_action "INSTALANDO PANDOC"
+  install_pandoc
 
-    log_info_action "INSTALANDO FERRAMENTAS DE CONEXÃO"
-    install_conectivity
+  log_info_action "INSTALANDO FERRAMENTAS DE CONEXÃO"
+  install_conectivity
 
-    log_info_action "INSTALANDO FERRAMENTAS DE AUDIO"
-    install_config_audio
+  log_info_action "INSTALANDO FERRAMENTAS DE AUDIO"
+  install_config_audio
 
-    log_info_action "INSTALANDO ARANDR PARA MULTIPLOS MONITORES"
-    install_arandr
+  log_info_action "INSTALANDO ARANDR PARA MULTIPLOS MONITORES"
+  install_arandr
 
-    log_info_action "INSTALANDO FEH" 
-    install_feh
+  log_info_action "INSTALANDO FEH" 
+  install_feh
 
-    log_info_action "INSTALANDO CURL"
-    install_curl
+  log_info_action "INSTALANDO CURL"
+  install_curl
 
-    log_info_action "INSTALANDO FONTS"
-    install_all_fonts
+  log_info_action "INSTALANDO FONTS"
+  install_all_fonts
 
-    log_info_action "INSTALANDO NEOVIM"
-    install_nvim
+  log_info_action "INSTALANDO NEOVIM"
+  install_nvim
 
-    log_info_action "INSTALANDO TERM-CLIENT GITLAB"
-    install_termlab
+  log_info_action "INSTALANDO TERM-CLIENT GITLAB"
+  install_termlab
 
-    log_info_action "INSTALANDO TERM-CLIENT GITHUB"
-    install_termhub
+  log_info_action "INSTALANDO TERM-CLIENT GITHUB"
+  install_termhub
 
-    log_info_action "INSTALANDO BROWSERS"
-    install_browsers
+  log_info_action "INSTALANDO BROWSERS"
+  install_browsers
 
-    log_info_action "INSTALANDO ATOM"
-    install_atom
+  log_info_action "INSTALANDO ATOM"
+  install_atom
 
-    log_info_action "INSTALANDO ZATHURA"
-    install_zathura
+  log_info_action "INSTALANDO ZATHURA"
+  install_zathura
 
-    log_info_action "INSTALANDO GIT FLOW"
-    install_git_flow
+  log_info_action "INSTALANDO GIT FLOW"
+  install_openssh
 
-    log_info_action "INSTALANDO DOCKER"
-    install_docker
+  log_info_action "INSTALANDO GIT FLOW"
+  install_git_flow
+
+  log_info_action "INSTALANDO DOCKER"
+  install_docker
 }
 
 option_java(){
-    log_info_option "JAVA"
+  log_info_option "JAVA"
 
-    log_info_action "INSTALANDO JDK"
-    install_jdk
+  log_info_action "INSTALANDO JDK"
+  install_jdk
 }
 
 option_node(){
-    log_info_option "NODE"
+  log_info_option "NODE"
 
-    log_info_action "INSTALANDO NODE"
+  log_info_action "INSTALANDO NODE"
 }
 
 option_php(){
-    log_info_option "PHP"
+  log_info_option "PHP"
 
-    log_info_action "INSTALANDO PHP"
-    install_php
+  log_info_action "INSTALANDO PHP"
+  install_php
 
-    log_info_action "INSTALANDO COMPOSER"
-    install_composer
+  log_info_action "INSTALANDO COMPOSER"
+  install_composer
 }
 
 option_python3(){
-    log_info_option "INSTALANDO PYTHON3"
+  log_info_option "INSTALANDO PYTHON3"
 
-    log_info_action "INSTALANDO PYTHON3"
-    install_python3
+  log_info_action "INSTALANDO PYTHON3"
+  install_python3
 }
 
 option_go(){
-    log_info_option "GO"
+  log_info_option "GO"
 
-    log_info_action "INSTALANDO INSTALANDO GO"
-    install_go
+  log_info_action "INSTALANDO INSTALANDO GO"
+  install_go
 }
 
 option_dev_extra(){
-    log_info_option "EXTRA PARA DESENVOLVIMENTO"
+  log_info_option "EXTRA PARA DESENVOLVIMENTO"
 
-    log_info_action "INSTALANDO LATEX"
-    install_latex
+  log_info_action "INSTALANDO LATEX"
+  install_latex
 }
 
 option_user_extra(){
-    log_info_option "CONFIGURAÇÕES CASUAIS EXTRAS"
+  log_info_option "CONFIGURAÇÕES CASUAIS EXTRAS"
 
-    log_info_action "INSTALANDO DISCORD"
-    install_discord
+  log_info_action "INSTALANDO DISCORD"
+  install_discord
 
-    log_info_action "INSTALANDO CALIBRE"
-    install_calibre
+  log_info_action "INSTALANDO CALIBRE"
+  install_calibre
 
-    log_info_action "INSTALANDO INKSCAPE"
-    install_inkscape
+  log_info_action "INSTALANDO INKSCAPE"
+  install_inkscape
 
-    log_info_action "INSTALANDO GIMP"
-    install_gimp
+  log_info_action "INSTALANDO GIMP"
+  install_gimp
 
-    log_info_action "INSTALANDO TRANSMISSION"
-    install_transmission
+  log_info_action "INSTALANDO TRANSMISSION"
+  install_transmission
 }
 # ---------------------------------------------------------- #
 
@@ -188,55 +191,55 @@ option_user_extra(){
 install_whiptail
 
 whiptail --title "IS2UM" --checklist --separate-output \
-    "↓, ↑, <space>, <tab> and <enter> to confirm"\
-    20 70 12 \
-    "dev-java" "" OFF \
-    "dev-node" "" OFF \
-    "dev-php" "" ON \
-    "dev-python3" "" ON \
-    "dev-go" "" ON \
-    "dev-extra" "latex e umbrello" OFF \
-    "user-extra" "discord, calibre, inkscape e gimp" OFF \
-    2>logwhiptail.txt
+  "↓, ↑, <space>, <tab> and <enter> to confirm"\
+  20 70 12 \
+  "dev-java" "" OFF \
+  "dev-node" "" OFF \
+  "dev-php" "" ON \
+  "dev-python3" "" ON \
+  "dev-go" "" ON \
+  "dev-extra" "latex e umbrello" OFF \
+  "user-extra" "discord, calibre, inkscape e gimp" OFF \
+  2>logwhiptail.txt
 
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    option_base
+  option_base
 
-    # ler e executar cenarios escolhidos
-    while read choice
-    do
-        case $choice in
-            "dev-java")
-                option_java
-                ;;
-            "dev-node")
-                option_node
-                ;;
-            "dev-php")
-                option_php
-                ;;
-            "dev-python3")
-                coption_python3
-                ;;
-            "dev-go")
-                option_go
-                ;;
-            "dev-extra")
-                option_dev_extra
-                ;;
-            "user-extra")
-                option_user_extra
-                ;;
-        esac
-    done < logwhiptail.txt
-    log_info_action "UPDATE / CLEAN"
-    install_all_from_ppa
-    sudo -E apt-get update
-    sudo aptget autoremove
-    sudo apt-get autoclean
-    sudo apt-get clean
-    log_info_action "INSTALAÇÃO TERMINADA!"
+  # ler e executar cenarios escolhidos
+  while read choice
+  do
+    case $choice in
+      "dev-java")
+        option_java
+        ;;
+      "dev-node")
+        option_node
+        ;;
+      "dev-php")
+        option_php
+        ;;
+      "dev-python3")
+        coption_python3
+        ;;
+      "dev-go")
+        option_go
+        ;;
+      "dev-extra")
+        option_dev_extra
+        ;;
+      "user-extra")
+        option_user_extra
+        ;;
+    esac
+  done < logwhiptail.txt
+  log_info_action "UPDATE / CLEAN"
+  install_all_from_ppa
+  sudo -E apt-get update
+  sudo aptget autoremove
+  sudo apt-get autoclean
+  sudo apt-get clean
+  log_info_action "INSTALAÇÃO TERMINADA!"
 else
-    log_warn "CANCELADO"
+  log_warn "CANCELADO"
 fi
